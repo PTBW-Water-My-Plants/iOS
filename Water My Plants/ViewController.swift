467842b2aa7e243220c4153f48f4d56a8b273d7a
bohdan462 <tech.bohdan@gmail.com>
2020-10-13 23:06:05 -0500

    Initial commit

---
+++ Water My Plants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Water My Plants
+//
+//  Created by Bohdan Tkachenko on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
